09b553112f44a1d1adfed72370a67287e7e6e6d2
Advait Krishna <amadvait007@gmail.com>
2025-04-22 11:31:38 +0000

    Runtime Error
    Error: Failed to load chunk server/chunks/ssr/[root of the server]__5a7e3f7b._.js
    
    Call Stack
    26
    
    Show 25 ignore-listed frame(s)
    Object.<anonymous>
    .next/server/app/page.js (6:9)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    config.output = {
+      ...config.output,
+      publicPath: '/',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
+
